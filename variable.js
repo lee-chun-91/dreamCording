3947e29005738683014dacd5e464b88f72af5d44
lee chun <lcg5320@gmail.com>
2022-03-16 18:19:04 +0900

    자바스크립트 3. 데이터타입, data types, let vs var, hoisting | 프론트앤드 개발자 입문편 (JavaScript ES5+)

--- variable.js
+++ variable.js
@@ -14,11 +14,95 @@ let globalName = 'global name';
     console.log(name);
     console.log(globalName);
 }
-console.log(name);
+console.log(name);~
 console.log(globalName);
 
 // var (don't ever use this!)
 // var hoisting (move declaration from bottom to top)
 // has no block scope
-age = 4;
 let age;
+age = 4;
+
+
+// 3. contants
+// favor immutable data type always for a few reasons:
+// - security
+// - thread safety
+// - reduce human mistakes
+
+const daysInWeek = 7;
+const maxNumber = 5;
+
+
+// 4. Variable types
+// primitive, single item: number, string, boolean, null, undefiendn, symbol
+// object, box container
+// function, first=class function
+
+const count = 17; // integer
+const size = 17.1; // decimal number
+console.log(`value: ${count}, type: ${typeof count}`); // value : 17, type: number
+console.log(`value: ${size}, type: ${typeof size}`); // value : 17.1, type: number
+
+// number - speicla numeric values: infinity, -infinity, NaN 
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+// bigInt (fairly new, don't use it yet)
+const bigInt = 12345678901234567890123456789011234567890n; // over (-2**53) ~ 2**53)
+console.log(`value: ${bigInt}, type: ${typeof bigInt}`);
+Number.MAX_SAFE_INTEGER;
+
+// string
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello ' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brendan}!`; // template literals (string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+console.log('value: ' + helloBob + ' type: ' + typeof helloBob);
+
+// boolean
+// false: 0, null, undefined, NaN, ''
+// true: any other value
+const canRead = true;
+const test = 3 < 1; // false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2); // false
+const gsymbol1 = Symbol.for('id');
+const gsymbol2 = Symbol.for('id');
+console.log(gsymbol1 === gsymbol2); // true
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`) //symbol은 .description 을 사용해서 string 으로 변환하고 출력해야 함
+
+// object, real-life object, data structure
+const ellie = { name: 'ellie', age: 20 };
+ellie.age = 21;
+
+
+// 5. Dynamic typing: dynamically typed language
+let text = 'hello';
+console.log(text.charAt(0)); //h
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '8' / '2';
+console.log(`value: ${text}, type: ${typeof text}`);
