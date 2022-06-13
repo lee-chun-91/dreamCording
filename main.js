adeca55dfb9c798ba57bcbdb7d4acdbc9c5ce71a
lee_chun <lcg5320@gmail.com>
2022-06-13 18:59:03 +0900

    Feat: add 'use strict' (added ECMAScript 5)

---
+++ main.js
@@ -0,0 +1,9 @@
+// whole-script strict mode syntax
+// JavaScript is very flexible
+// flexible === dangerous
+// added ECMAScript 5
+"use strict";
+
+console.log("hello world!");
+
+console.log("test");
